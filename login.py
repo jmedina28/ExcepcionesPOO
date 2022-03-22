c54a6d22922874bfe66b1e570ab2dea2aa5ac8c9
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-22 16:15:08 +0100

    doy formato al código

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 import re
 import time
 
+
 class Login():
 
     def comprobacion(correo, tiempo, intentos):
@@ -16,20 +17,22 @@ class Login():
                 Login.comprobacion(
                     str(input("\nIntroduzca una dirección de correo electrónico correcta: ")), tiempo, intentos)
             else:
-                print("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
+                print(
+                    "Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
                 Login.comprobacion(
                     str(input("\nIntroduzca una dirección de correo electrónico: ")), tiempo, intentos)
 
         elif tiempo < 10:
             tiempo += 5
             print("\n---> Para evitar ciberataques usted no dispone de más intentos en los próximos " +
-                str(tiempo) + " segundos.")
+                  str(tiempo) + " segundos.")
             time.sleep(tiempo)
             Login.comprobacion(
                 str(input("\nIntroduzca una dirección de correo electrónico: ")), tiempo, 0)
         else:
-            print("Usted ha agotado todas las oportunidades que tenía disponibles, vuelva a intentarlo más tarde.")
+            print(
+                "Usted ha agotado todas las oportunidades que tenía disponibles, vuelva a intentarlo más tarde.")
 
 
 Login.comprobacion(
-        str(input("Introduzca una dirección de correo electrónico: ")), 0, 0)
+    str(input("Introduzca una dirección de correo electrónico: ")), 0, 0)
