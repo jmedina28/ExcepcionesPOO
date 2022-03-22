bbf7d6c7d6f4de5e5198a957e56188e8e9c36820
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-22 14:46:30 +0100

    sigo añadiendo cosas pero ahora da error

--- login.py
+++ login.py
@@ -1,23 +1,28 @@
 import re
-correo = str(input("Introduzca una dirección de correo electrónico: "))
+import time 
 
-def comprobacion(correo): 
-    intentos = 0
-    tiempo = 0
-    while True:
-        intentos += 1
-        if intentos <= 10:    
-            if (re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", correo)):
-                print("Correo válido.")
-                break
-            elif correo == "":
-                print("Introduzca su correo electrónico por favor.")
-                correo = str(input("Introduzca una dirección de correo electrónico: "))
-            else:
-                print("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
-                correo = str(input("Introduzca una dirección de correo electrónico: "))
+correo = str(input("Introduzca una dirección de correo electrónico: "))
+tiempo = 0
+intentos = 0
+
+def comprobacion(correo,tiempo,intentos): 
+    
+    intentos += 1
+    if intentos <= 10:    
+        if (re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", correo)):
+            print("Correo válido. Bienvenido:)")
+            
+        elif correo == "" or correo == " ":
+            print("Introduzca su correo electrónico por favor.")
+            comprobacion(str(input("Introduzca una dirección de correo electrónico: ")), tiempo, intentos)
         else:
-            print("Para evitar ciberataques usted no dispone de más intentos en los próximos " + str(tiempo) + " segundos")
-            break
+            print("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
+            comprobacion(str(input("Introduzca una dirección de correo electrónico: ")), tiempo, intentos)
+    else:
+        tiempo += 8
+        print("Para evitar ciberataques usted no dispone de más intentos en los próximos " + str(tiempo) + " segundos")
+        time.sleep(tiempo)
+        comprobacion(str(input("Introduzca una dirección de correo electrónico: ")), tiempo, intentos)
+            
         
-comprobacion(correo)
+comprobacion(correo,tiempo,intentos)
