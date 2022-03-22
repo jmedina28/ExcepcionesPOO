99f936adf5f188dfcf5c23c5df11d366a19a5562
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-22 14:12:56 +0100

    añadiendo mejoras al código

--- login.py
+++ login.py
@@ -1,7 +1,12 @@
 import re
 correo = str(input("Introduzca una dirección de correo electrónico: "))
 
-if (re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", correo)):
-    print("Correo válido.")
-else:
-    print("Introduzca un correo electrónico válido por favor.")
+def comprobacion(correo): 
+    if (re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", correo)):
+        return "Correo válido."
+    elif correo == "":
+        return "Introduzca su correo electrónico por favor."
+    else:
+        return "Una dirección de correo electrónico debe tener el formato xxx@xxx.xx"
+    
+print(comprobacion(correo))
