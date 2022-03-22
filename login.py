0198d6b25f17f5b276634a635a519237498f6925
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-22 16:04:28 +0100

    doy formato al documento

--- login.py
+++ login.py
@@ -1,30 +1,36 @@
 import re
-import time 
+import time
 
 correo = str(input("Introduzca una dirección de correo electrónico: "))
 tiempo, intentos = 0, 0
 
-def comprobacion(correo,tiempo,intentos): 
-    
 
-    if intentos <= 5:    
-        intentos += 1
+def comprobacion(correo, tiempo, intentos):
+
+    intentos += 1
+    if intentos <= 5:
+
         if (re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", correo)):
             print("Correo válido. Bienvenido:)")
-            
+
         elif correo == "":
             print("Introduzca su correo electrónico por favor.")
-            comprobacion(str(input("Introduzca una dirección de correo electrónico: ")),tiempo,intentos)
+            comprobacion(
+                str(input("Introduzca una dirección de correo electrónico: ")), tiempo, intentos)
         else:
             print("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
-            comprobacion(str(input("Introduzca una dirección de correo electrónico: ")),tiempo,intentos)
+            comprobacion(
+                str(input("Introduzca una dirección de correo electrónico: ")), tiempo, intentos)
+
     elif tiempo < 15:
         tiempo += 5
-        print("Para evitar ciberataques usted no dispone de más intentos en los próximos " + str(tiempo) + " segundos")
-        time.sleep(tiempo) 
-        comprobacion(str(input("Introduzca una dirección de correo electrónico: ")),tiempo,0)
+        print("Para evitar ciberataques usted no dispone de más intentos en los próximos " +
+              str(tiempo) + " segundos")
+        time.sleep(tiempo)
+        comprobacion(
+            str(input("Introduzca una dirección de correo electrónico: ")), tiempo, 0)
     else:
         print("Usted ha agotado todas las oportunidades que tenía disponibles, vuelva a intentarlo más tarde.")
-            
-        
-comprobacion(correo,tiempo,intentos)
+
+
+comprobacion(correo, tiempo, intentos)
