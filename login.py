441d09c3a26925e9d4333700c0c403ffe1c419df
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-22 14:26:05 +0100

    vamos añadiendo detalles al código

--- login.py
+++ login.py
@@ -2,11 +2,22 @@ import re
 correo = str(input("Introduzca una dirección de correo electrónico: "))
 
 def comprobacion(correo): 
-    if (re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", correo)):
-        return "Correo válido."
-    elif correo == "":
-        return "Introduzca su correo electrónico por favor."
-    else:
-        return "Una dirección de correo electrónico debe tener el formato xxx@xxx.xx"
-    
-print(comprobacion(correo))
+    intentos = 0
+    tiempo = 0
+    while True:
+        intentos += 1
+        if intentos <= 10:    
+            if (re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", correo)):
+                print("Correo válido.")
+                break
+            elif correo == "":
+                print("Introduzca su correo electrónico por favor.")
+                correo = str(input("Introduzca una dirección de correo electrónico: "))
+            else:
+                print("Una dirección de correo electrónico debe tener el formato xxx@xxx.xx")
+                correo = str(input("Introduzca una dirección de correo electrónico: "))
+        else:
+            print("Para evitar ciberataques usted no dispone de más intentos en los próximos " + str(tiempo) + " segundos")
+            break
+        
+comprobacion(correo)
